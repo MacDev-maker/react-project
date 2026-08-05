3818e82df7e13e2988955e5f326f4afcdbaaf0f9
Maciej <wmaciek%^@gmail.com>
2026-08-05 18:02:43 +0200

    Adding welcome section on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Header from './components/Header';
+import WelcomeSection from './components/WelcomeSection';
+import circle from './assets/circle.svg';
 import './App.css';
 
 function App() {
   return (
     <>
       <title>MW site</title>
+      <link rel="icon" type="image/svg+xml" href={circle} />
 
       <Header />
+
+      <WelcomeSection />  
     </>
   )
 }
---
+++ src/components/WelcomeSection.tsx
@@ -0,0 +1,23 @@
+import './WelcomeSection.css';
+
+function WelcomeSection() {
+  return (
+    <>
+      <div className='fullname-container'>
+        <div className='name'>
+          Maciej
+        </div>
+
+        <div className='surname'>
+          Węglarz
+        </div>
+      </div>
+
+      <p className='my-informations'>
+        Computer Science Student @ University of the National Education Commission, Krakow React • TypeScript • JavaScript | AWS • Vitest • Jasmine <br/> Networking Basics | AI for Everyone
+      </p>
+    </>
+  );
+}
+
+export default WelcomeSection
